e0b24d882cd3d4637349fcc9939f5714124ae55a
lilyeggy <2217386257@qq.com>
2026-03-12 17:31:09 +0800

    use ZhipuAI/GLM-5 model

--- spirit/core/config.py
+++ spirit/core/config.py
@@ -34,7 +34,7 @@ class Settings(BaseSettings):
     OPENAI_MODEL: str = "gpt-4"
     
     MODELSCOPE_API_KEY: Optional[str] = None
-    MODELSCOPE_MODEL: str = "glm-5"
+    MODELSCOPE_MODEL: str = "ZhipuAI/GLM-5"
     USE_MODELSCOPE: bool = False
     
     DEFAULT_REVIEW_PERIOD: str = "weekly"
